ca3e4cab6635fc93916ee0bd19c073abf65c54ea
Sawmon Abo <sawmonabo@Sawmons-MacBook-Pro.local>
2023-12-16 21:30:24 -0500

    Adds example conig

---
+++ example.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  appName: 'InstaClone',
+  appLogo: require('./assets/instaclone-logo.png'),
+  firebase: {
+    apiKey: '...',
+    authDomain: '...',
+    projectId: '...',
+    storageBucket: '...',
+    messagingSenderId: '...',
+    appId: '...',
+    measurementId: '...',
+  },
+}
